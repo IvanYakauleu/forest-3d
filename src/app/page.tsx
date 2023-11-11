68206d3873cc4cc1f94f81c4fdbd0c0293ae145a
Ivan Yakauleu <voodoolar333@gmail.com>
2023-11-11 21:38:28 +0100

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,60 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client';
+
+import RainCanvas from '../Components/Rain/Rain';
+import { useRef, useEffect } from 'react';
 
 export default function Home() {
+  const mouseRef = useRef<HTMLDivElement>(null);
+
+  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+    // setPosition({ x: e.clientX, y: e.clientY });
+    if (mouseRef.current) {
+      mouseRef.current.style.setProperty(
+        '--move-x',
+        `${(e.clientX - window.innerWidth / 2) * -0.005}deg`,
+      );
+      mouseRef.current.style.setProperty(
+        '--move-y',
+        `${(e.clientY - window.innerHeight / 2) * -0.01}deg`,
+      );
+    }
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main ref={mouseRef} onMouseMove={handleMouseMove}>
+      <div className="logo" style={{ backgroundImage: 'url("/logo.svg")' }}></div>
+      <section className="layers">
+        <div className="layers__container">
+          <div
+            className="layers__item layer-1"
+            style={{ backgroundImage: 'url(/layer-1.jpg)' }}></div>
+          <div
+            className="layers__item layer-2"
+            style={{ backgroundImage: 'url(/layer-2.png)' }}></div>
+          <div className="layers__item layer-3">
+            <div className="hero-content">
+              <h1>
+                NATURAL FOREST <span>HTML / CSS</span>
+              </h1>
+              <div className="hero-content__p">
+                Creating a beautiful 3D website with a ‘lens effect’
+              </div>
+              <button className="button-start">Start Learning</button>
+            </div>
+          </div>
+          <div className="layers__item layer-4">
+            <RainCanvas />
+          </div>
+          <div
+            className="layers__item layer-5"
+            style={{ backgroundImage: 'url(/layer-5.png)' }}></div>
+          <div
+            className="layers__item layer-6"
+            style={{ backgroundImage: 'url(/layer-6.png)' }}></div>
         </div>
-      </div>
+      </section>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <div style={{ height: '100vh' }}></div>
     </main>
-  )
+  );
 }
